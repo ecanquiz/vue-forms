691add846b67a531b0cebe28cdc2881ab611973a
Ernesto Canquiz <caribestic123@gmail.com>
2024-03-27 12:11:28 -0400

    My first commit

---
+++ src/components/AppTextarea.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import useUniqueId from '../composables/useUniqueId'
+
+withDefaults(defineProps<{
+  label?: string
+  modelValue?: string | number
+  error?: string
+}>(), {
+  label: '',
+  modelValue: '',
+  error: ''
+})
+
+const uuid = useUniqueId().getID()
+</script>
+
+<template>
+  <label v-if="label">{{ label }}</label>
+  <textarea    
+    v-bind="$attrs"
+    :value="modelValue"
+    :placeholder="label"
+    @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"    
+    class="field"
+    :id="uuid"
+    :aria-describedby="error ? `${uuid}-error` : undefined"
+    :aria-invalid="error ? true : undefined"
+  ></textarea>
+  <AppErrorMessage
+    v-if="error"
+    :id="`${uuid}-error`"
+  >
+    {{ error }}
+  </AppErrorMessage>
+</template>
---
+++ src/index.ts
@@ -0,0 +1,56 @@
+import { defineAsyncComponent } from 'vue'
+
+const AppBtn = defineAsyncComponent(() => import('./components/AppBtn.vue'))
+const AppCheckbox = defineAsyncComponent(() => import('./components/AppCheckbox.vue'))
+const AppErrorMessage = defineAsyncComponent(() => import('./components/AppErrorMessage.vue'))
+const AppFlashMessage = defineAsyncComponent(() => import('./components/AppFlashMessage.vue'))
+const AppInConstruction = defineAsyncComponent(() => import('./components/AppInConstruction.vue'))
+const AppInput = defineAsyncComponent(() => import('./components/AppInput.vue'))
+const AppLabelSpan = defineAsyncComponent(() => import('./components/AppLabelSpan.vue'))
+const AppLink = defineAsyncComponent(() => import('./components/AppLink.vue'))
+const AppLoadingButton = defineAsyncComponent(() => import('./components/AppLoadingButton.vue'))
+const AppPageHeader = defineAsyncComponent(() => import('./components/AppPageHeader.vue'))
+const AppPaginationA = defineAsyncComponent(() => import('./components/AppPaginationA.vue'))
+const AppPaginationB = defineAsyncComponent(() => import('./components/AppPaginationB.vue'))
+const AppPaginationC = defineAsyncComponent(() => import('./components/AppPaginationC.vue'))
+const AppPending = defineAsyncComponent(() => import('./components/AppPending.vue'))
+const AppRadio = defineAsyncComponent(() => import('./components/AppRadio.vue'))
+const AppRadioGroup = defineAsyncComponent(() => import('./components/AppRadioGroup.vue'))
+const AppSectionBorder = defineAsyncComponent(() => import('./components/AppSectionBorder.vue'))
+const AppSectionInfo = defineAsyncComponent(() => import('./components/AppSectionInfo.vue'))
+const AppSelect = defineAsyncComponent(() => import('./components/AppSelect.vue'))
+const AppTextarea = defineAsyncComponent(() => import('./components/AppTextarea.vue'))
+
+export {
+  AppBtn,
+  AppCheckbox,
+  AppErrorMessage,
+  AppFlashMessage,
+  AppInConstruction,
+  AppInput,
+  AppLabelSpan,
+  AppLink,
+  AppLoadingButton,
+  AppPageHeader,
+  AppPaginationA,
+  AppPaginationB,
+  AppPaginationC,
+  AppPending,
+  AppRadio,
+  AppRadioGroup,
+  AppSectionBorder,
+  AppSectionInfo,  
+  AppSelect,
+  AppTextarea
+};
+
+  
+
+  
+  
+  
+
+
+
+
+
